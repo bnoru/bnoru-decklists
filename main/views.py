85d37ce55af10011b8c4478329c368b609ba88a4
Bruno <bnoru156@gmail.com>
2021-01-07 02:36:02 -0300

    views and actual home.html

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Decklist
 
 def homepage(request):
-    return HttpResponse("Habemus sitem")
-# Create your views here.
+    return render (request=request,
+                   template_name="main/home.html",
+                   context={"decklists": Decklist.objects.all})
