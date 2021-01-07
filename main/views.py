7ddb67d8bacc2b36e2b3547f1a7f09b4c3a91e33
Bruno <bnoru156@gmail.com>
2021-01-07 02:57:22 -0300

    views db

--- main/views.py
+++ main/views.py
@@ -6,3 +6,12 @@ def homepage(request):
     return render (request=request,
                    template_name="main/home.html",
                    context={"decklists": Decklist.objects.all})
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings})
